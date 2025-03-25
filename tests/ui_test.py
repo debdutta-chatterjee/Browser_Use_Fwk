84e1450f69db44453f1219c0b43419697389d882
Debdutta Chatterjee <debduttachatterjee09@gmail.com>
2025-03-25 22:31:14 +0530

    baseline pytest

--- tests/ui_test.py
+++ tests/ui_test.py
@@ -26,23 +26,36 @@ class CheckoutResult(BaseModel):
     confirmation_message:str
     url: str
 
-
-def test_ui_flow(sample_fixture):
+@pytest.mark.asyncio
+async def test_ui_flow(sample_fixture):
     
-    file_content = FileUtil.read_file_to_string("./test_cases/tc_0001_ui_flow.feature")
-    print(file_content)
-
-    steps = FileUtil.extract_steps(file_content)
-    print(steps)
-
     load_dotenv()
     gemini_api_key = os.environ["GEMINI_API_KEY"]
-    gemini =GeminiLLM(gemini_api_key)
-    llm = gemini.get_llm_model
 
-    
+    task =(
+        'I am UI automation tester validating the tasks'
+        'open the website url "https://rahulshettyacademy.com/loginpagePractise/"'
+        'Login with username and password, Login details available in the same page'
+        'Get attribute and url of the page'
+        'After login, select first 2 products and them to cart'
+        'Then checkout and store the total value you see on the screen'
+        'Increase the quantity of any product and check if total value is updated accordingly'
+        'checkout and select country, agree terms and purchase'
+        'verify thank you message is displayed'
+    )
+
+
+    class CheckoutResult(BaseModel):
+        login_status:str
+        cart_status: str
+        checkout_status:str
+        total_update_status:str
+        delivery_location_status:str
+        confirmation_message:str
+        url: str
+
     controller = Controller(output_model=CheckoutResult)
-    
+
     @controller.action(description='open the website url')
     async def open_website(browser: BrowserContext):
         page = await browser.get_current_page()
@@ -56,9 +69,18 @@ def test_ui_flow(sample_fixture):
         attr = await page.get_by_text('Shop Name').get_attribute('class')
         print(url)
         return ActionResult(extracted_content="url is {url} and attribute is {attr}")
-    
-    browser_use = BrowserAgent(steps,controller,llm)
-    agent = browser_use.get_agent()
+    llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash-exp",api_key =gemini_api_key)
 
-    print("\nExecuting test case")
-    assert 2 + 2 == 4  # Example assertion
+    agent = Agent(
+        task= task,
+        llm=llm,
+        controller=controller
+        #,use_vision = true
+    )
+    history = await agent.run()
+    history.save_to_file('result.json')
+    test_result = history.final_result()
+    result = CheckoutResult.model_validate_json(test_result)
+    print(test_result)
+    assert result.confirmation_message == "Thank you! Your order will be delivered in next few weeks :-)."
+
